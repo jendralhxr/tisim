0f2d86fab7740a04b7de02eeba284e747b29bfa8
jendralhxr <jendral.hxr@gmail.com>
2019-10-02 14:45:51 +0900

    yolo works @30 fps w/ color imagingsource camera

--- tisim-yolo.cpp
+++ tisim-yolo.cpp
@@ -4,7 +4,7 @@
  *   
  *  compile against OpenCV
  *   $	g++ tisim-send.cpp -o tisim-send `pkg-config opencv --libs` 
- *   $	./tisim-send <camera-device> <receiver-host> <port>
+ *   $	./tisim-yolo <camera-device> <receiver-host> <port>
  * 
  * adapted from:
  * https://gist.github.com/Circuitsoft/1126411
@@ -25,9 +25,15 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
+#include <thread>
 
 #include "PracticalSocket.h"      // For UDPSocket and SocketException
 #include "config.h"
+
+// darknet yolo detection w/ opencv
+#define GPU
+#define OPENCV
+#include "yolo_v2_class.hpp"
 
 using namespace std;
 using namespace cv;
@@ -53,8 +59,26 @@ int total_pack;
 string servAddress;
 unsigned short servPort;
 UDPSocket sock;
-vector < int > compression_params;
-vector < uchar > encoded;
+vector <int> compression_params;
+vector <uchar> encoded;
+
+std::vector<std::string> objects_names_from_file(std::string const filename) {
+    std::ifstream file(filename);
+    std::vector<std::string> file_lines;
+    if (!file.is_open()) return file_lines;
+    for(std::string line; getline(file, line);) file_lines.push_back(line);
+    std::cout << "object names loaded \n";
+    return file_lines;
+}
+
+
+//yolo
+std::string  names_file = "coco.names";
+std::string  cfg_file = "yolov3-tiny.cfg";
+std::string  weights_file = "yolov3-tiny.weights";
+vector <string> obj_names = objects_names_from_file(names_file); 
+Detector detector(cfg_file, weights_file);
+//float const thresh =  0.2;
             
 static int xioctl(int fd, int request, void *arg){
 	int r;
@@ -175,6 +199,91 @@ int set_props(char *device){
 	sprintf(command, "v4l2-ctl -d %s -c brightness=%d", device, BRIGHTNESS);
 	system(command);
 	}
+
+void draw_boxes(cv::Mat mat_img, std::vector<bbox_t> result_vec, std::vector<std::string> obj_names,
+    int current_det_fps = -1, int current_cap_fps = -1)
+{
+    int const colors[6][3] = { { 1,0,1 },{ 0,0,1 },{ 0,1,1 },{ 0,1,0 },{ 1,1,0 },{ 1,0,0 } };
+
+    for (auto &i : result_vec) {
+        cv::Scalar color = obj_id_to_color(i.obj_id);
+        cv::rectangle(mat_img, cv::Rect(i.x, i.y, i.w, i.h), color, 2);
+        if (obj_names.size() > i.obj_id) {
+            std::string obj_name = obj_names[i.obj_id];
+            if (i.track_id > 0) obj_name += " - " + std::to_string(i.track_id);
+            cv::Size const text_size = getTextSize(obj_name, cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, 2, 0);
+            //cv::Size const text_size = getTextSize(obj_name, cv::FONT_HERSHEY_COMPLEX_SMALL, 0, 0, 0);
+            
+            int max_width = (text_size.width > i.w + 2) ? text_size.width : (i.w + 2);
+            max_width = std::max(max_width, (int)i.w + 2);
+            //max_width = std::max(max_width, 283);
+            std::string coords_3d;
+            if (!std::isnan(i.z_3d)) {
+                std::stringstream ss;
+                ss << std::fixed << std::setprecision(2) << "x:" << i.x_3d << "m y:" << i.y_3d << "m z:" << i.z_3d << "m ";
+                coords_3d = ss.str();
+                cv::Size const text_size_3d = getTextSize(ss.str(), cv::FONT_HERSHEY_COMPLEX_SMALL, 0.8, 1, 0);
+                int const max_width_3d = (text_size_3d.width > i.w + 2) ? text_size_3d.width : (i.w + 2);
+                if (max_width_3d > max_width) max_width = max_width_3d;
+            }
+
+            cv::rectangle(mat_img, cv::Point2f(std::max((int)i.x - 1, 0), std::max((int)i.y - 35, 0)),
+                cv::Point2f(std::min((int)i.x + max_width, mat_img.cols - 1), std::min((int)i.y, mat_img.rows - 1)),
+                color, CV_FILLED, 8, 0);
+            //putText(mat_img, obj_name, cv::Point2f(i.x, i.y - 16), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, cv::Scalar(0, 0, 0), 2);
+            putText(mat_img, obj_name, cv::Point2f(i.x, i.y - 16), cv::FONT_HERSHEY_COMPLEX_SMALL, 0.8, cv::Scalar(0, 0, 0), 2);
+            if(!coords_3d.empty()) putText(mat_img, coords_3d, cv::Point2f(i.x, i.y-1), cv::FONT_HERSHEY_COMPLEX_SMALL, 0.8, cv::Scalar(0, 0, 0), 1);
+        }
+    }
+    if (current_det_fps >= 0 && current_cap_fps >= 0) {
+        std::string fps_str = "FPS detection: " + std::to_string(current_det_fps) + "   FPS capture: " + std::to_string(current_cap_fps);
+        putText(mat_img, fps_str, cv::Point2f(10, 20), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, cv::Scalar(50, 255, 0), 2);
+    }
+}
+
+void show_console_result(std::vector<bbox_t> const result_vec, std::vector<std::string> const obj_names, int frame_id = -1) {
+    std::cout << std::endl;
+    for (auto &i : result_vec) {
+        if (obj_names.size() > i.obj_id) std::cout << obj_names[i.obj_id] << " - ";
+        std::cout << "obj_id = " << i.obj_id << ",  x = " << i.x << ", y = " << i.y
+            << ", w = " << i.w << ", h = " << i.h
+            << std::setprecision(3) << ", prob = " << i.prob << std::endl;
+    }
+}
+
+
+template<typename T>
+class send_one_replaceable_object_t {
+    const bool sync;
+    std::atomic<T *> a_ptr;
+public:
+
+    void send(T const& _obj) {
+        T *new_ptr = new T;
+        *new_ptr = _obj;
+        if (sync) {
+            while (a_ptr.load()) std::this_thread::sleep_for(std::chrono::milliseconds(3));
+        }
+        std::unique_ptr<T> old_ptr(a_ptr.exchange(new_ptr));
+    }
+
+    T receive() {
+        std::unique_ptr<T> ptr;
+        do {
+            while(!a_ptr.load()) std::this_thread::sleep_for(std::chrono::milliseconds(3));
+            ptr.reset(a_ptr.exchange(NULL));
+        } while (!ptr);
+        T obj = *ptr;
+        return obj;
+    }
+
+    bool is_object_present() {
+        return (a_ptr.load() != NULL);
+    }
+
+    send_one_replaceable_object_t(bool _sync) : sync(_sync), a_ptr(NULL)
+    {}
+};
 	 
 int capture_image(int fd){
     if(-1 == xioctl(fd, VIDIOC_QBUF, &buf))
@@ -217,20 +326,18 @@ int capture_image(int fd){
     //color
     memmove(raw.data, buffer, sizeof(char)*FRAME_SIZE);
     cvtColor(raw, image, CV_BayerBG2BGR);
-//    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
-//    imshow( "Display window", image );                   // Show our image inside it.
-//	waitKey(1);                                          // Wait for a keystroke in the window
+	// namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+	// imshow( "Display window", image );                   // Show our image inside it.
+	// waitKey(1);                                          // Wait for a keystroke in the window
 
     // grey
-   //memmove(raw.data, buffer, sizeof(char)*FRAME_SIZE);
-   //imencode(".jpg", raw, encoded, compression_params);
-   //imshow( "Display window", raw );                   
-	
-	//------------yolo here on  "image"----------------
-	
-	
-	
-	
+	// memmove(raw.data, buffer, sizeof(char)*FRAME_SIZE);
+	// imencode(".jpg", raw, encoded, compression_params);
+	// imshow( "Display window", raw );                   
+	return 0;
+}
+     
+int send_image(){
 	// color
 	// here goes transmitting routine
 	imencode(".jpg", image, encoded, compression_params);
@@ -247,15 +354,14 @@ int capture_image(int fd){
 		//~ printf("sending content %d/%d size:%d\n", i+1, total_pack, PACK_SIZE);
     }
 	
-//    waitKey(1);
-//    waitKey(FRAME_INTERVAL);
+	// waitKey(1);
+	// waitKey(FRAME_INTERVAL);
 	
-    //char filename[20];
-    //sprintf(filename, "/tmp/walo.jpg");
-    //imwrite(filename, image);
-    return 0;
-}
-     
+    // char filename[20];
+    // sprintf(filename, "/tmp/walo.jpg");
+    // imwrite(filename, image);
+	}
+	
 int main(int argc, char **argv){
 
 	buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
@@ -276,11 +382,13 @@ int main(int argc, char **argv){
 		return 1;
 		}  
 	
-	set_props(argv[1]);
+	set_props(argv[1]); // setting camera properties
      
     compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
     compression_params.push_back(ENCODE_QUALITY);
-
+	auto obj_names = objects_names_from_file(names_file);
+	
+    
 	try{
 	servAddress = argv[2]; // First arg: server address
     servPort = Socket::resolveService(argv[3], "udp");
@@ -289,6 +397,15 @@ int main(int argc, char **argv){
 	gettimeofday(&start,NULL);
 	while(1){
 		if(capture_image(fd)) return 1;
+		else {
+			std::vector<bbox_t> result_vec = detector.detect(image);
+			draw_boxes(image, result_vec, obj_names);
+			//show_console_result(result_vec, obj_names);
+			cv::imshow("window name", image);
+			cv::waitKey(20);
+			send_image();
+			}
+		
 		frames_count++;
 		gettimeofday(&stop,NULL);
 		
